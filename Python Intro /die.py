ee2605c9399bb85b8781c34598764a0f76757261
Tiffany <tar3820@gmail.com>
2015-08-12 23:54:58 -0700

    Update my die class and the html example. Also add slide and Angry Dice.

--- Python Intro /die.py
+++ Python Intro /die.py
@@ -1,36 +1,46 @@
 # This is a dice faking module
-from random import randint
-
-def roll(max):
-   r = randint(1, max)
-   return r
-
-def roll_a_bunch(max, numOfDice=4):
-   rolls = []
-   for i in range(numOfDice):
-      rolls.append(roll(max))
-
-   return rolls
+from random import choice
 
 
-def roll_distro(max, numOfDice=4):
-   #Roll a bunch of dice
-   rolls = roll_a_bunch(max, numOfDice)
+class Die():
 
-   distribution = {}
+   def __init__(self, possibleValues):
+      self.possibleValues = possibleValues
+      self.currentValue = choice(self.possibleValues)
 
-   # count what's rolled
-   for each in rolls:
-      currentCount = distribution.get(each, 0)
-      print("Current count of", each, ":",currentCount)
-      currentCount += 1
-      distribution[each] = currentCount
+   def roll(self):
+      self.currentValue = choice(self.possibleValues)
+      return self.currentValue
 
-   output = ""
-   for roll in distribution:
-      output += "Number " + str(roll) + " was rolled " + str(distribution[roll]) + " times\n"
+   def __repr__(self):
+      return self.currentValue
 
-   print(output)
+   def roll_a_bunch(self, max, numOfDice=4):
+      rolls = []
+      for i in range(numOfDice):
+         rolls.append(roll(max))
+
+      return rolls
+
+
+   def roll_distro(self, max, numOfDice=4):
+      #Roll a bunch of dice
+      rolls = roll_a_bunch(max, numOfDice)
+
+      distribution = {}
+
+      # count what's rolled
+      for each in rolls:
+         currentCount = distribution.get(each, 0)
+         print("Current count of", each, ":",currentCount)
+         currentCount += 1
+         distribution[each] = currentCount
+
+      output = ""
+      for roll in distribution:
+         output += "Number " + str(roll) + " was rolled " + str(distribution[roll]) + " times\n"
+
+      print(output)
 
 
 
