60f2daf0ec2aa8e0fc4ba9b836cf87a6c1d237a0
kielejocain <joecken@gmail.com>
2015-08-20 14:02:43 -0700

    get player move tests

--- Python Intro /Connect4Assignment/tests/test_get_player_move.py
+++ Python Intro /Connect4Assignment/tests/test_get_player_move.py
@@ -8,9 +8,7 @@ from connect4_view import View
 
 
 class Connect4GetPlayerMoveTest(unittest.TestCase):
-    """
-    Testing the View's get_player_move function.
-    """
+    """Test the Get Player Move method from the View"""
 
     def setUp(self):
         self.view = View()
@@ -18,9 +16,43 @@ class Connect4GetPlayerMoveTest(unittest.TestCase):
     def tearDown(self):
         del self.view
 
+    @patch('builtins.input', return_value="1")
+    def test_with_no_message(self, inputted_value):
+        """
+        Ensures that the view correctly handles a standard move input.
+        :param inputted_value: string passed in by mock.patch
+        :return: NoneType
+        """
+        return_value = self.view.get_player_move()
+        self.assertEqual(return_value, 1, "get_player_move should return 1")
+
+    @patch('builtins.input', return_value="3")
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_with_message(self, inputted_value, output):
+        """
+        Ensures that the view correctly handles a passed in message.
+        :param inputted_value: string passed in by mock.patch
+        :param output: string printed to stdout by the function
+        :return: NoneType
+        """
+        return_value = self.view.get_player_move(msg="Not so much.")
+        self.assertEqual(
+            output.getvalue(),
+            "Not so much.\n",
+            "Message is not printing correctly"
+        )
+        self.assertEqual(return_value, 3, "get_player_move should return 3")
+
     @patch('builtins.input', side_effect=["", "4"])
     @patch('sys.stdout', new_callable=StringIO)
-    def test_with_no_message(self, inputted_value, output):
+    def test_with_no_response(self, inputted_value, output):
+        """
+        Ensures that the view will reject an empty string as a move
+        and press for a new answer.
+        :param inputted_value: strings passed in by mock.patch
+        :param output: strings printed to stdout by the function
+        :return: NoneType
+        """
         return_value = self.view.get_player_move()
         self.assertEqual(
             output.getvalue(),
@@ -28,6 +60,16 @@ class Connect4GetPlayerMoveTest(unittest.TestCase):
             "Empty string warning is incorrect"
         )
         self.assertEqual(return_value, 4, "get_player_move should return 4")
+
+    @patch('builtins.input', side_effect=["10", "-3", "2"])
+    def test_with_invalid_int(self, inputted_value):
+        """
+        Ensures that the view properly rejects invalid integers.
+        :param inputted_value:
+        :return:
+        """
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
