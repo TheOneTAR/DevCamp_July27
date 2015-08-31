f5ec5223bb7a9345880da740856e7fdca6c14224
williamsmichael <willia32@hotmail.com>
2015-08-31 11:30:55 -0700

    validates each form field

--- javapic/js/script_join.js
+++ javapic/js/script_join.js
@@ -1,8 +1,36 @@
 // A login form that won’t let users continue until their inputs are correct
 // Make tagline Develop something beautiful, {name}
 
-var validateType = {
-  email: function(el) {
-  };
+var form = document.getElementById("signup");
+var inputs = document.getElementsByTagName("input");
+var nameInput = document.querySelector("input[name=name]")
+var username = document.querySelector("input[name=username]")
+var email = document.querySelector("input[type=email]")
+
+nameInput.addEventListener("blur", function(){ checkName(); });
+username.addEventListener("blur", function(){ checkUsername(); });
+email.addEventListener("blur", function(){ checkEmail(); });
+
+function checkName() {
+  if (nameInput.value.length < 2) {
+    alert('Please provide a name of two or more characters.')
+    nameInput.focus;
+  }
+}
+
+function checkUsername() {
+  if (username.value.length < 2) {
+    alert('Please provide a username of two or more characters.')
+  username.focus;
+  }
+}
+
+function checkEmail() {
+  var valid = /[^@]+@[^@].+/;
+  if (!valid.test(email.value)) {
+    alert('Please provide a valid email address.\n' +
+      'yourEmail@place.com');
+    email.focus;
+  }
 }
 
